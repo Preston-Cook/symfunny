1b6dca4b126a3c754ae64c5a21730b44154a5781
Preston-Cook <preston.l.cook@gmail.com>
2024-04-06 13:31:54 -0500

    finish nav and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,31 @@
 import { Button } from './ui/button';
 import Link from 'next/link';
+import { Cedarville_Cursive as FontCursive } from 'next/font/google';
+import { Music } from 'lucide-react';
+
+const fontSans = FontCursive({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export default async function Footer() {
   return (
     <footer className="border-t sm:flex sm:justify-between py-8 px-4 border-b bg-secondary mt-6">
       <div className="mx-auto max-w-screen-xl text-center">
         <Link href="/">
-          <Button className="sm:pl-0 text-2xl font-semibold" variant="link">
-            Symfunny
+          <Button
+            className={`sm:pl-0 text-2xl font-semibold ${fontSans.className}`}
+            variant="link"
+          >
+            <p>Symfunny</p>
+            <p className="ml-3">
+              <Music />
+            </p>
           </Button>
         </Link>
-        <span className="text-sm sm:text-center ">
+        <div className="text-sm sm:text-center mt-3">
           © 2024 Symfunny™ All Rights Reserved
-        </span>
+        </div>
       </div>
     </footer>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,31 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import HamburgerMenu from './HamburgerMenu';
 import DarkModeToggle from './DarkModeToggle';
+import { Cedarville_Cursive as FontCursive } from 'next/font/google';
+import { Music } from 'lucide-react';
+
+const fontSans = FontCursive({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 async function Header() {
   return (
     <header className="sm:flex sm:justify-between py-3 px-4 border-b bg-secondary">
       <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full">
-        <div className="flex items-center">
-          <Link href="/">
-            <h1 className="text-2xl font-bold text-primary">Velago</h1>
-          </Link>
-        </div>
+        <Link href="/">
+          <div className="flex items-center w-[140px] justify-between">
+            <h1
+              className={`text-2xl font-bold text-primary ${fontSans.className}`}
+            >
+              Symfunny
+            </h1>
+            <div>
+              <Music />
+            </div>
+          </div>
+        </Link>
         <nav className="space-x-6 hidden max-w-400px justify-end md:flex ml-auto"></nav>
         <div className="flex items-center justify-end">
           <div className="hidden md:inline-flex mx-6">
