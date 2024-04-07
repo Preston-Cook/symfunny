e1f2002695ff1caa93333814b27306e4bd69aef5
Preston-Cook <preston.l.cook@gmail.com>
2024-04-07 17:58:09 -0500

    change responsiveness for no results page

--- src/components/NoResults.tsx
+++ src/components/NoResults.tsx
@@ -15,9 +15,9 @@ export default function NoResults({ query, creator, isEmpty }: NoResultsProps) {
     <section>
       <div className="px-4 bg-secondary border rounded-md py-4 w-[60%] lg:w-[40%] mx-auto">
         <div className="text-center">
-          <h1 className="mb-4 text-4xl tracking-tight font-bold text-primary flex items-center justify-center gap-4">
-            <p>{isEmpty ? 'No Sounds in Database' : 'No Results Found'}</p>{' '}
-            <CircleOff size={30} className="" />
+          <h1 className="mb-4 text-xl md:text-2xl lg:text-3xl tracking-tight font-bold text-primary flex items-center justify-center gap-4">
+            {isEmpty ? 'No Sounds in Database' : 'No Results Found'}
+            <CircleOff className="hidden sm:inline-block" />
           </h1>
           <p className="mb-4 text-lg font-light my-4 w-[80%] mx-auto">
             {isEmpty ? 'Be the First to Upload' : description}
